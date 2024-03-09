a1526f940885816f9446512aa79c49f3aaae9842
pwrignall <pwrignall@users.noreply.github.com>
2024-03-09 17:41:31 +0000

    Try adding CNAME

--- .eleventy.js
+++ .eleventy.js
@@ -25,6 +25,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"assets/.well-known": ".well-known"});
   eleventyConfig.addPassthroughCopy({ "node_modules/simplelightbox/dist/simple-lightbox.min.css": "simplelightbox/simple-lightbox.min.css" });
   eleventyConfig.addPassthroughCopy({ "node_modules/simplelightbox/dist/simple-lightbox.min.js": "simplelightbox/simple-lightbox.min.js" });
+  eleventyConfig.addPassthroughCopy({ "assets/CNAME": "CNAME"})
 
   //Since moving the CSS inline eleventy no longer watches it (because it's not being copied to output), so I had to include it as a watch target.
   //Adding it to addPassthroughCopy also means it's not watched.
